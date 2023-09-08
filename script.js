569f71b7efd3b6e424b136f9fd316f9db8296698
wild-fox <kunpenggk@gmail.com>
2023-09-08 16:42:30 +0800

    增加底部页目条，添加增加与滚动功能

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
 import { Gantt } from "dhtmlx-gantt";
 
 gantt.config.xml_date = "%Y-%m-%d %H:%i";
-gantt.init("gantt_here");
+gantt.init("ganttHere");
 gantt.parse({
     data: [
         { id: 1, text: "Project #1", start_date: null, duration: null, parent: 0, progress: 0, open: true },
@@ -17,4 +17,36 @@ gantt.parse({
         { id: 2, source: 3, target: 4, type: "0" },
         { id: 3, source: 5, target: 6, type: "0" }
     ]
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const footer = document.getElementById("footer");
+    /* 添加footer的滚动响应 */
+    footer.addEventListener("wheel", function (event) {
+        this.scrollLeft += (event.deltaY * 1); // 你可以调整这里的40来控制滚动速度
+        // 防止页面本身的滚动
+        event.preventDefault();
+    });
+
+
+
+    /* 为按钮添加点击事件 */
+    const addBtn = document.getElementById("addBtn");
+
+    let btnCount = 0;
+
+    addBtn.addEventListener("click", function () {
+        // 自动生成按钮ID
+        const newBtnId = "dataBtn" + (++btnCount);
+
+        // 创建新按钮
+        const newBtn = document.createElement("button");
+        newBtn.id = newBtnId;
+        newBtn.className = "dataBtn";
+        newBtn.innerText = "新数据" + btnCount;
+
+        // 在 addBtn 之前插入新按钮
+        footer.insertBefore(newBtn, addBtn);
+    });
 });

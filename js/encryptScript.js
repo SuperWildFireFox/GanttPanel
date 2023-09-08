4f1ac325218557b9450704ffc193a19154ac2175
wild-fox <kunpenggk@gmail.com>
2023-09-08 23:58:34 +0800

    完成初步的加密功能

---
+++ js/encryptScript.js
@@ -0,0 +1,63 @@
+const forge = require('node-forge');
+
+// 预先加密的数据和 AES 密钥
+//AES加密后的firebaseConfig
+const encryptedFirebaseConfig = 'dVz/EsAY97tOih1THNWRSAwAL09EeQotpWz2ZOLQtiSzbAnWZ0aXf/bJ8qPczhJjPQe5BP5uQvQFqyhBFaKF4N4ENx2rVQpijH5cKXvgtWh5uLRyZlfsq5JmJh10reiMFOBedsb91PzAzmW0cPw2mdn9D9eMXtWeEPaRKG5aG6ULiVjWhRGUW05qlWljtb/+uGyyGsQoYNeNJOuWsatNjC0YPFPjMXr8kaCs0qZ+JhoEt1eSTTCXtMwwUMvwqRSm7BYx7em+O7RpByQUGWrQWYJ2eiqdfRCzfos/bWxNRD6czQBOzJcOKqJz1vxDubXRsel+RWefn+sxcPUC28H6R9z5JdvSNi1fb7QUjayqSD5azWYc6juPVTkdyToSriQQAADOXDeoRXVdggbGsprDHxeCLZtJxGViSJGG8awVpEXE1pzAcQComfgwcuVG/LubyMzLXXV4gsfQJc4FI+GkIb5Be4x9DW6jTy3OElRNj8I=';  // 使用 AES 加密后的 firebaseConfig
+//RSA加密后的AES密钥
+const encryptedAESKey = 'T3QYRNVa/Ov1ETQ24uKI36hJYxWcRNKkk0ZnstncymXkAa7jd9Zd1elwlOeOGgxQaWm0W4hGiIM41uUtUGEWpj0Ds0iF3+YdyP9JPzBm6gLaUDuD/rmDTXF84Tj/HyDXBHTGAxdNUu0QOap3Nve7DbHcHlfiGdV+g6MbBSsZwoXV5cXxmMdCj4rmKdFXSBIDmW5yQZYqdf7Gxbavl5zOdg861aMhaaIfuBBkZ11qODo4kFDkI+ZlUURoq/Cxq1BGET+ck08gPLi5TGZyD5WVQRZ70rFvqfli2KHilX4umZzT8R8X1txDFpIGUIfDbYhrSooy3cszH860j4zMoOrbNw==';  // 使用 RSA 加密后的 AES 密钥
+
+// 解密 AES 密钥
+function decryptRSA(privateKey, encryptedKey) {
+    const decoded = forge.util.decode64(encryptedKey);
+    return privateKey.decrypt(decoded, 'RSA-OAEP');
+}
+
+// 使用 AES 解密数据
+function decryptAES(key, encryptedText) {
+    const decipher = forge.cipher.createDecipher('AES-ECB', key);
+    decipher.start();
+    decipher.update(forge.util.createBuffer(forge.util.decode64(encryptedText)));
+    decipher.finish();
+    return decipher.output.toString();
+}
+
+// 检查私钥是否能成功解密数据
+function checkKey() {
+    try {
+        //格式要求很严格，开头与结尾的换行都不能缺
+        const enteredPrivateKeyPem = '\n' + document.getElementById('passDialogInput').value;
+        // console.log('私钥:', enteredPrivateKeyPem);
+        const enteredPrivateKey = forge.pki.privateKeyFromPem(enteredPrivateKeyPem);
+        // console.log('私钥对象:', enteredPrivateKey);
+
+        const decryptedAESKey = decryptRSA(enteredPrivateKey, encryptedAESKey);
+        // console.log('成功解密 AES 密钥:', decryptedAESKey);
+        const decryptedFirebaseConfig = decryptAES(decryptedAESKey, encryptedFirebaseConfig);
+        // console.log('成功解密 firebaseConfig 数据:', decryptedFirebaseConfig);
+
+        document.cookie = `privateKey=${encodeURIComponent(enteredPrivateKeyPem)};`;
+        document.getElementById('passDialog').style.display = 'none';
+        // console.log('成功解密:', JSON.parse(decryptedFirebaseConfig));
+    } catch (e) {
+        // console.log(e)
+        alert('解密失败或密钥错误');
+        document.getElementById('passDialog').style.display = 'block';
+    }
+}
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById("passDialogButton");
+    button.addEventListener('click', checkKey);
+});
+
+// 初始化
+window.onload = function () {
+    const cookiePrivateKey = document.cookie.split('; ').find(row => row.startsWith('privateKey='));
+
+    if (cookiePrivateKey) {
+        const privateKeyPem = decodeURIComponent(cookiePrivateKey.split('=')[1]);
+        document.getElementById('passDialogInput').value = privateKeyPem;
+        checkKey();
+    } else {
+        document.getElementById('passDialog').style.display = 'block';
+    }
+};
